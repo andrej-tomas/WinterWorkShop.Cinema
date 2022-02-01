2c4c3bb438f14787ba4f9eb912e5562db8e6df13
Andrej Tomas <andrejtomas@aol.com>
2022-02-01 14:38:19 +0100

    Added a ProjectionModel.

---
+++ WinterWorkShop.Cinema.Data/Models/ProjectionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WinterWorkShop.Cinema.Data.Models
+{
+    public class ProjectionModel
+    {
+        public int Id { get; set; } 
+        public DateTime ProjectionDate { get; set; }    
+        public string CinemaName { get; set; }
+
+    }
+}
